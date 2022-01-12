1d610b63bb83b56f515f20200bf7fdd25554ff2d
Camdyn Rasque <rasque.camdyn@gmail.com>
2022-01-11 16:05:55 -0800

    Updated to HTML5

--- scripts/webcomponent.js
+++ scripts/webcomponent.js
@@ -1,10 +1,12 @@
 customElements.define(
-	'hello-wc',
-	class extends HTMLElement {
-		constructor() {
-			super();
-			var template = document.getElementById('hello-wc-template').content;
-			const shadowRoot = this.attachShadow({ mode: 'open' }).appendChild(template.cloneNode(true));
-		}
-	}
+  'hello-wc',
+  class extends HTMLElement {
+    constructor() {
+      super();
+      var template = document.getElementById('hello-wc-template').content;
+      const shadowRoot = this.attachShadow({ mode: 'open' }).appendChild(
+        template.cloneNode(true)
+      );
+    }
+  }
 );
