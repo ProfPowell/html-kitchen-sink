811de04e3f04006d2d0a173f66e656069b1f6196
Prof Thomas Powell <47124258+ProfPowell@users.noreply.github.com>
2022-01-11 16:16:34 -0800

    Update webcomponent.js
    var to let ... come on it's 2022!

--- scripts/webcomponent.js
+++ scripts/webcomponent.js
@@ -3,7 +3,7 @@ customElements.define(
   class extends HTMLElement {
     constructor() {
       super();
-      var template = document.getElementById('hello-wc-template').content;
+      let template = document.getElementById('hello-wc-template').content;
       const shadowRoot = this.attachShadow({ mode: 'open' }).appendChild(
         template.cloneNode(true)
       );
